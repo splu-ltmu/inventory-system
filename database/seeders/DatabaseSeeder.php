236077b45f76d1a53f962acad2402e9653009270
allandaryl.dev <allandaryl.dev@gmail.com>
2026-05-07 15:57:02 +0800

    heaps seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,12 +17,21 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // User::factory(10)->create();
+        $data = [
+            [
+                'name' => 'admin User',
+                'email' => 'dev@allan.com',
+                'password'   => Hash::make(env('ADMIN_PASSWORD', 'admin123')),
+                'role' => 'admin'
+            ],
+            [
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'dev@allan.com',
-            'password'   => Hash::make(env('ADMIN_PASSWORD', 'admin123')),
-            'role'=>'admin'
-        ]);
+                'name' => 'client User',
+                'email' => 'dev@client.com',
+                'password'   => Hash::make(env('ADMIN_PASSWORD', 'admin123')),
+                'role' => 'client'
+            ]
+        ];
+        User::factory()->createMany($data);
     }
 }
