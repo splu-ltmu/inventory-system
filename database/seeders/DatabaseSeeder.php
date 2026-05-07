db9aa69034366ff7d580664f6d690afc644d18eb
allandaryl.dev <allandaryl.dev@gmail.com>
2026-05-07 13:44:57 +0800

    heaps

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -19,7 +20,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'dev@allan.com',
+            'password'   => Hash::make(env('ADMIN_PASSWORD', 'admin123')),
         ]);
     }
 }
